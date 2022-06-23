35f22a2cabc564697edf712fa13b422104bfadba
Lau Yee-Yu <liuyiyu2003@126.com>
2022-06-24 01:19:46 +0800

    feat: the front getter

---
+++ include/reorder_buffer.h
@@ -0,0 +1,62 @@
+// RISC-V Simulator
+// Copyright (C) 2022 Lau Yee-Yu
+//
+// This library is free software: you can redistribute it and/or modify
+// it under the terms of the GNU General Public License as published by
+// the Free Software Foundation, either version 3 of the License, or
+// any later version.
+//
+// This program is distributed in the hope that it will be useful,
+// but WITHOUT ANY WARRANTY; without even the implied warranty of
+// MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+// GNU General Public License for more details.
+//
+// You should have received a copy of the GNU General Public License
+// along with this program.  If not, see <https://www.gnu.org/licenses/>.
+
+#ifndef RISC_V_SIMULATOR_INCLUDE_REORDER_BUFFER_H
+#define RISC_V_SIMULATOR_INCLUDE_REORDER_BUFFER_H
+
+#include "circular_queue.h"
+#include "type.h"
+
+enum class ReorderType {
+    registerWrite,
+    wordMemoryWrite, // write a word to memory
+    halfWordMemoryWrite, // write a half word to memory
+    byteMemoryWrite, // write a byte to memory
+    branch
+};
+
+struct ReorderBufferEntry {
+    bool ready = false;
+    bool predictedAnswer = false;
+    ReorderType type;
+    SizeType index;
+    WordType offset; // only for memory write
+    WordType value;
+};
+
+/**
+ * @class ReorderBuffer
+ * The ReorderBuffer class is used to store the instructions in the reorder
+ * buffer (RoB).
+ */
+class ReorderBuffer {
+public:
+    ReorderBuffer() = default;
+    ReorderBuffer(const ReorderBuffer&) = default;
+    ReorderBuffer(ReorderBuffer&&) = default;
+
+    ReorderBuffer& operator=(const ReorderBuffer&) = default;
+    ReorderBuffer& operator=(ReorderBuffer&&) = default;
+
+    ~ReorderBuffer() = default;
+
+    void Commit(SizeType index);
+
+private:
+    CircularQueue<ReorderBufferEntry, 32> queue_;
+};
+
+#endif //RISC_V_SIMULATOR_INCLUDE_REORDER_BUFFER_H
